a733f8c5e1a639697341661933cfa95164967d60
MyEngine-Official <myengine.official@outlook.com>
2026-03-12 18:13:14 -0400

    QUITAR OPENCODE

---
+++ YotsubaEngine/Forms/Contract/IButton.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace YotsubaEngine.Forms.Contract
+{
+    public interface IButton : ChildElement.Forms
+    {
+        public event Action OnClick;
+    }
+}
---
+++ YotsubaEngine/Forms/Contract/ILabel.cs
@@ -0,0 +1,9 @@
+﻿namespace YotsubaEngine.Forms.Contract
+{
+    public interface ILabel : ChildElement.Forms
+    {
+        public void DrawImGuI();
+        public void DrawGumUI();
+        public void DrawMyra();
+    }
+}
